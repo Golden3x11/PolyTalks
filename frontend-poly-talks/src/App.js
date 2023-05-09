81fac3efcda1c10945e9d305e1b2b435a0be581c
Golden3x11 <72710770+Golden3x11@users.noreply.github.com>
2023-05-09 11:26:22 +0200

    Merge pull request #1
    * file upload on front

--- frontend-poly-talks/src/App.js
+++ frontend-poly-talks/src/App.js
@@ -3,6 +3,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import { createTheme, ThemeProvider } from '@mui/material';
 import { red } from '@mui/material/colors';
 import { Header } from './components/Header';
+import {FileUploadComponent} from "./components/FileUploadComponent";
 
 const theme = createTheme({
   palette: {
@@ -20,6 +21,7 @@ function App() {
     <GoogleOAuthProvider clientId={'407712005402-sumiqjgamkhccs0flh9o6p26gijrmepn.apps.googleusercontent.com'}>
       <ThemeProvider theme={theme}>
         <Header/>
+        <FileUploadComponent/>
       </ThemeProvider>
     </GoogleOAuthProvider>
   );
