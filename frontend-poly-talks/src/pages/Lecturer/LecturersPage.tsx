cb5b1722e05334803c3452e9b4894a92e1bd9916
Golden3x11 <72710770+Golden3x11@users.noreply.github.com>
2023-05-19 14:03:26 +0200

    added search to course/lecturer/thread

--- frontend-poly-talks/src/pages/Lecturer/LecturersPage.tsx
+++ frontend-poly-talks/src/pages/Lecturer/LecturersPage.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import {LecturerDto} from "../../dto/lecturer.dto";
 import {makeStyles} from "tss-react/mui";
-import {Divider, List} from "@mui/material";
+import {Divider, IconButton, Input, List, Paper} from "@mui/material";
 import {Link} from "react-router-dom";
+import SearchIcon from "@mui/icons-material/Search";
 
 const useStyles = makeStyles()((theme) => ({
     red: {
@@ -13,7 +14,13 @@ const useStyles = makeStyles()((theme) => ({
 export const LecturersPage = () => {
     const {classes, cx} = useStyles(undefined, undefined);
     const [lecturers, setLecturer] = React.useState<LecturerDto[]>([])
+    const [searchTerm, setSearchTerm] = useState('');
 
+    const filteredLecturers = lecturers.filter(l => (l.name +" "+ l.surname).toLowerCase().includes(searchTerm.toLowerCase()));
+
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event.target.value);
+    };
 
     React.useEffect(() => {
         fetch('http://localhost:8080/api/lecturer')
@@ -25,8 +32,17 @@ export const LecturersPage = () => {
     return (
         <div style={{margin: '0 2em', display: 'flex', flexDirection: 'column', gap: '0.3em'}}>
             <h1 className={`${classes.red}`} style={{fontWeight: 'bold'}}>Prowadzący</h1>
+            <Paper
+                component="form"
+                sx={{p: '2px 4px', display: 'flex', alignItems: 'center', width: 400, justifyContent: "space-between"}}
+            >
+                <Input sx={{flexGrow: 1}} type="text" value={searchTerm} onChange={handleSearchChange} placeholder="Szukaj po tytule"/>
+                <IconButton type="button" sx={{p: '10px'}} aria-label="search">
+                    <SearchIcon/>
+                </IconButton>
+            </Paper>
             <List component="nav">
-                {lecturers.map(lecturer => (<>
+                {filteredLecturers.map(lecturer => (<>
                         <Link style={{textDecoration: "none", color: "black"}} to={`/lecturer/${lecturer._id}`}>
                             <h3>{lecturer.name} {lecturer.surname}</h3>
                         </Link>
--- frontend-poly-talks/src/pages/ThreadsPage.tsx
+++ frontend-poly-talks/src/pages/ThreadsPage.tsx
@@ -1,9 +1,9 @@
-import {Divider, List} from '@mui/material';
-import React from "react";
+import {Divider, IconButton, Input, List, Paper} from '@mui/material';
+import React, {useState} from "react";
 import {makeStyles} from "tss-react/mui";
 import {ThreadDto} from "../dto/thread.dto";
 import {Link} from "react-router-dom";
-
+import SearchIcon from '@mui/icons-material/Search';
 
 const useStyles = makeStyles()((theme) => ({
     red: {
@@ -15,7 +15,13 @@ const useStyles = makeStyles()((theme) => ({
 export const ThreadsPage = () => {
     const {classes, cx} = useStyles(undefined, undefined);
     const [threads, setThreads] = React.useState<ThreadDto[]>([])
+    const [searchTerm, setSearchTerm] = useState('');
 
+    const filteredThreads = threads.filter(thread => thread.title.toLowerCase().includes(searchTerm.toLowerCase()));
+
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event.target.value);
+    };
 
     React.useEffect(() => {
         fetch('http://localhost:8080/api/thread')
@@ -28,13 +34,21 @@ export const ThreadsPage = () => {
     return (
         <div style={{margin: '0 2em', display: 'flex', flexDirection: 'column', gap: '0.3em'}}>
             <h1 className={`${classes.red}`} style={{fontWeight: 'bold'}}>Wątki</h1>
+            <Paper
+                component="form"
+                sx={{p: '2px 4px', display: 'flex', alignItems: 'center', width: 400, justifyContent: "space-between"}}
+            >
+                <Input sx={{flexGrow: 1}} type="text" value={searchTerm} onChange={handleSearchChange} placeholder="Szukaj po tytule"/>
+                <IconButton type="button" sx={{p: '10px'}} aria-label="search">
+                    <SearchIcon/>
+                </IconButton>
+            </Paper>
             <List component="nav">
-                {threads.map(thread => (<>
+                {filteredThreads.map(thread => (<>
                         <Link style={{textDecoration: "none", color: "black"}} to={`/threads/${thread._id}`}>
                             <h3>{thread.title}</h3>
                             <p>{thread.description}</p>
-                        </Link>
-                        <Divider/>
+                        </Link><Divider/>
                     </>
                 ))}
             </List>
