66d21516864a23b635f0dc67b474f29151195e5c
meex28 <piotrp7392@gmail.com>
2023-05-30 20:25:13 +0200

    Add sidebar display based on loggedin user

--- frontend-poly-talks/src/components/navbars/Sidebar.tsx
+++ frontend-poly-talks/src/components/navbars/Sidebar.tsx
@@ -7,6 +7,8 @@ import MenuBookIcon from '@mui/icons-material/MenuBook';
 import PersonIcon from '@mui/icons-material/Person';
 import TagIcon from '@mui/icons-material/Tag';
 import './../../styles/navbars.css';
+import { useContext } from 'react';
+import { AuthContext } from '../../authentication/Authentication';
 
 const useStyles = makeStyles()((theme) => ({
   container: {
@@ -30,6 +32,7 @@ export const Sidebar = () => {
   const { classes, cx } = useStyles(undefined, undefined);
   const innerStacksSpacing = 2;
   const outerStackSpacing = 4;
+  const {currentUser} = useContext(AuthContext)
 
   return (
     <Stack
@@ -40,7 +43,9 @@ export const Sidebar = () => {
       <Stack spacing={innerStacksSpacing}>
         <Typography className={`${classes.firstChild} ${classes.header}`}>Tematy</Typography>
         <IconText text={"Na czasie"} Icon={CallMadeIcon} route={"/"}/>
-        <IconText text={"Ulubione"} Icon={StarIcon} route={"/favourites"}/>
+        {currentUser &&
+          <IconText text={"Ulubione"} Icon={StarIcon} route={"/favourites"} />
+        }
       </Stack>
 
       <Stack spacing={innerStacksSpacing}>
@@ -50,12 +55,14 @@ export const Sidebar = () => {
         <IconText text={"Inne"} Icon={TagIcon} route={"/threads"}/>
       </Stack>
 
-      <Stack spacing={innerStacksSpacing}>
-        <Typography className={classes.header}>Dodaj</Typography>
-        <IconText text={"Kurs"} Icon={MenuBookIcon} route={"/courses/create"}/>
-        <IconText text={"Prowadzącego"} Icon={PersonIcon} route={"/lecturers/create"}/>
-        <IconText text={"Wątek"} Icon={TagIcon} route={"/threads/create"}/>
-      </Stack>
+      {currentUser &&
+        <Stack spacing={innerStacksSpacing}>
+          <Typography className={classes.header}>Dodaj</Typography>
+          <IconText text={"Kurs"} Icon={MenuBookIcon} route={"/courses/create"} />
+          <IconText text={"Prowadzącego"} Icon={PersonIcon} route={"/lecturers/create"} />
+          <IconText text={"Wątek"} Icon={TagIcon} route={"/threads/create"} />
+        </Stack>
+      }
 
     </Stack>
   );
